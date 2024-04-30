6bd88f9b9991d1894e5514ba03208674e166dcc1
malvikpandey <malvikpandey@gmail.com>
2024-04-30 18:22:41 +0530

    Updated

---
+++ Sprint-10/Day-2/conditionalStatements1.js
@@ -0,0 +1,50 @@
+//Problem-1
+var a = 10;
+var b = 20;
+
+if(a > b){
+    console.log(a);
+}
+else{
+    console.log(b);
+}
+
+//Problem-2
+var age = 25;
+
+if(age > 18){
+    console.log("You are an adult");
+}
+else{
+    console.log("You are minor");
+}
+
+//Problem-3
+var a = 25;
+
+if(a % 5 == 0){
+    console.log("Divisible by 5");
+}
+else{
+    console.log("Not divisible by 5");
+}
+
+//Problem-4
+var a = 6;
+
+if(a % 2 == 0){
+    console.log("Number is even");
+}
+else{
+    console.log("Number is odd");
+}
+
+//Problem-5
+var a = -2;
+
+if(a > 0){
+    console.log("Positive");
+}
+else{
+    console.log("Not positive");
+}
